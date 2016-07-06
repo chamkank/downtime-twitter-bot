e3bf7b00cb5d2e7dba2f1d6a1e88990dd8bfd342
CK <zechamkank@users.noreply.github.com>
2016-07-06 11:41:58 -0700

    Implemented sleep time parameterization
    First part of suggestion by @AmmsA

--- bot.py
+++ bot.py
@@ -8,6 +8,7 @@ CONSUMER_SECRET = ''
 ACCESS_TOKEN = ''
 ACCESS_TOKEN_SECRET = ''
 WEBSITE = '' # URL of website to monitor
+POLLING_TIME = 60 # Interval (in seconds) to check if site is up
 DOWNTIME_MESSAGE = "Our site is currently down at the moment. Thank you for your patience." #Tweet for when site goes down
 DOWNTIME_FINISHED_MESSAGE = "Our site is back up! Thanks again for your patience." #Tweet for when site comes back up
 
@@ -17,7 +18,7 @@ auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
 api = tweepy.API(auth)
 
 
-def site_status(site):
+def site_is_up(site):
     try :
         return (urllib.request.urlopen(WEBSITE).getcode() == 200)
     except:
@@ -25,19 +26,22 @@ def site_status(site):
     
 def downtime_started():
     api.update_status(DOWNTIME_MESSAGE)
-    while (not site_status(WEBSITE)):
-        if site_status(WEBSITE):
+    while (not site_is_up(WEBSITE)):
+        if site_is_up(WEBSITE):
             downtime_finished()
-        time.sleep(60)
+        time.sleep(POLLING_TIME)
 
 def downtime_finished():
     api.update_status(DOWNTIME_FINISHED_MESSAGE)
     print("Site went back up at %r" % time.ctime())
 
-while True:        
-    if site_status(WEBSITE):
-        print("Site is up")
-        time.sleep(60)
-    else:
-        print("Site went down at %r" % time.ctime())
-        downtime_started()
+def main_loop():
+	while True:        
+		if site_is_up(WEBSITE):
+			print("Site is up")
+			time.sleep(POLLING_TIME)
+		else:
+			print("Site went down at %r" % time.ctime())
+			downtime_started()
+			
+main_loop()
